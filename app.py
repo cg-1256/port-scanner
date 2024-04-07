5ef6ccff4cae378e4885aba9dcbbff8edd3016e7
Raffi Rizzo <rizzoraffi@gmail.com>
2024-04-07 12:55:53 +0100

    add in some error checking

--- app.py
+++ app.py
@@ -1,22 +1,37 @@
-import socket
-import sys
+import socket, sys, os
+from datetime import datetime
+ 
+current_dir = os.getcwd()
+date = datetime.now()
+dir_addition = "port_scan_"+str(date)
+new_dir = os.path.join(current_dir, dir_addition)
+os.mkdir(new_dir)
+
+open_file = new_dir+"/open_ports.txt"
+closed_file = new_dir+"/closed_ports.txt"
 
 host = input("Enter host address: ")
-while True:
-    try:
-        low=int(input("Enter the low bound of ports to scan: "))
-        break
-    except ValueError:
-        print ("Invalid port number")
+low = -1
+high = -1
 
-while True:
-    try:
-        high=int(input("Enter the high bound of ports to scan: "))+1
-        break
-    except ValueError:
-        print ("Invalid port number")
+while low==-1 or low>65534:
+	while True:
+		try:
+			low=int(input("Enter the low bound of ports to scan: "))
+			break
+		except ValueError:
+			print ("Invalid port number")
+
+while high==-1 or high>65536 or high<=low:
+	while True:
+		try:
+			high=int(input("Enter the high bound of ports to scan: "))+1
+			break
+		except ValueError:
+			print ("Invalid port number")
 
 try:
+    
 	for port in range(low, high):
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		socket.setdefaulttimeout(1)
@@ -24,6 +39,11 @@ try:
 		result = sock.connect_ex((host,port))
 		if result == 0:
 			print("[+] Port {} is open".format(port))
+			with open(open_file, 'a') as file:
+				file.write("[+] Port {} is open\n".format(port))
+		else:
+			with open(closed_file, 'a') as file:
+				file.write("[-] Port {} is closed\n".format(port))
 		sock.close()
 except KeyboardInterrupt:
 	print("\nExiting program...")
